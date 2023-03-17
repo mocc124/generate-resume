8fdb54de5a893946b01532cb983d5e2c86384a4d
junnian <mrnianj@hotmail.com>
2023-03-17 12:52:48 +0800

    feat: 封装card组件以提高复用性

---
+++ src/components/card.vue
@@ -0,0 +1,27 @@
+<template>
+<div class='container'>
+    <h2 class="main-title">{{ title }}</h2>
+    <hr v-if="showHr"/>
+    <slot></slot>
+</div>
+</template>
+
+<script setup lang='ts'>
+import { ref,reactive,onMounted,defineProps } from 'vue';
+
+defineProps({
+    title: {
+        type: String,
+        default:"默认标题"
+    },
+    showHr: {
+        type:Boolean,
+        default:true
+    }
+})
+
+</script>
+
+<style scoped>
+
+</style>
--- src/components/view.vue
+++ src/components/view.vue
@@ -15,61 +15,70 @@
         <el-row>
         <el-col :span="2"><div class="grid-content ep-bg-purple" /></el-col>
         <el-col :span="20">
-          <h2 class="main-title">个人优势</h2>
-          <hr>
-          <el-descriptions title="基础技能" :column=1>
-            <el-descriptions-item  v-for="item in user.bases" :label="item.name">
-              {{ item.data.join(' 、') }}
-            </el-descriptions-item>
-          </el-descriptions>
-          <el-descriptions title="个人总结" :column=1>
-            <el-descriptions-item v-for="item in user.about">
-              {{item}}
-            </el-descriptions-item>
-          </el-descriptions>
-          <h2 class="main-title">工作经历</h2>
-          <hr>
-          <el-descriptions v-for="item in user.companyList" :title="`${item.name}（${item.time}）`" :column=1>
-            <el-descriptions-item label="公司简介">
-              {{ item.intro }}
-            </el-descriptions-item>
-            <el-descriptions-item label="主要负责的项目">
-              <ul>
-                <li v-for="list in item.projects">{{ list.name }}</li>
-              </ul>
-            </el-descriptions-item>
-            <el-descriptions-item label="工作内容">
-              {{ item.content }}
-            </el-descriptions-item>
-          </el-descriptions>
-          <h2 class="main-title">项目经历</h2>
-          <hr>
-          <el-descriptions v-for="item in user.projectList" :title="item.name" :column=1>
-            <el-descriptions-item label="技术栈">
-              <el-tag size="small"  v-for="list in item.skill" class="card-skill-item">
-                {{ list }}
-              </el-tag>
-            </el-descriptions-item>
-            <el-descriptions-item label="项目周期">
-              {{ item.time }}
-            </el-descriptions-item>
-            <el-descriptions-item label="业绩">
-              {{ item.content }}
-            </el-descriptions-item>
-            <el-descriptions-item label="项目难点">
-              {{ item.difficulty }}
-            </el-descriptions-item>
-            <el-descriptions-item label="项目介绍">
-              {{ item.intro }}
-            </el-descriptions-item>
-          </el-descriptions>
-          <h2 class="main-title">致谢</h2>
-          <!-- <hr> -->
-          <el-descriptions>
-            <el-descriptions-item>
-              {{ user.thanks }}
-            </el-descriptions-item>
-          </el-descriptions>
+          <CardVue title="个人优势">
+            <template v-slot>
+              <el-descriptions title="基础技能" :column=1>
+                <el-descriptions-item v-for="item in user.bases" :label="item.name">
+                  <el-tag v-for="value in item.data" class="tag">{{ value }}</el-tag>
+                </el-descriptions-item>
+              </el-descriptions>
+              <el-descriptions title="个人总结" :column=1>
+                <el-descriptions-item v-for="item in user.about">
+                  {{ item }}
+                </el-descriptions-item>
+              </el-descriptions>
+            </template>
+          </CardVue>
+          <CardVue title="工作经历">
+            <el-descriptions v-for="item in user.companyList" :title="`${item.name}（${item.time}）`" :column=1>
+              <el-descriptions-item label="简介">
+                {{ item.intro }}
+              </el-descriptions-item>
+              <!-- <el-descriptions-item label="主要负责的项目">
+                <ul>
+                  <li v-for="list in item.projects">{{ list.name }}</li>
+                </ul>
+              </el-descriptions-item> -->
+              <el-descriptions-item label="主要职责">
+                {{ item.content }}
+              </el-descriptions-item>
+            </el-descriptions>
+          </CardVue>
+          <CardVue title="项目经历">
+            <template v-slot>
+              <el-descriptions v-for="item in user.projectList" :title="item.name" :column=1>
+                <el-descriptions-item label="技术栈">
+                  <el-tag size="small"  v-for="list in item.skill" class="card-skill-item">
+                    {{ list }}
+                  </el-tag>
+                </el-descriptions-item>
+                <el-descriptions-item label="项目介绍">
+                  {{ item.intro }}
+                </el-descriptions-item>
+                <el-descriptions-item label="项目周期">
+                  {{ item.time }}
+                </el-descriptions-item>
+                <el-descriptions-item label="工作内容">
+                  <ul>
+                    <li v-for="(i,index) in item.content">{{ index+1 }}, {{ i }}</li>
+                  </ul>
+                </el-descriptions-item>
+                <el-descriptions-item label="项目难点">
+                  <ul>
+                    <li v-for="(i,index) in item.difficulty">{{ index+1 }}, {{ i }}</li>
+                  </ul>
+                </el-descriptions-item>
+                
+              </el-descriptions>
+            </template>
+          </CardVue>
+          <CardVue title="致谢" :show-hr=false>
+            <el-descriptions>
+              <el-descriptions-item>
+                {{ user.thanks }}
+              </el-descriptions-item>
+            </el-descriptions>
+          </CardVue>
         </el-col>
         <el-col :span="2"><div class="grid-content ep-bg-purple" /></el-col>
       </el-row>
@@ -82,13 +91,16 @@
   import { ref,reactive,toRaw,onMounted } from 'vue';
   import { useStore } from "@/stores/counter";
   import { useRouter } from "vue-router";
+  import CardVue from "./card.vue";
 
   let router = useRouter()
-  const {user} = useStore()
+
+  const { user } = useStore()
 
   onMounted(()=>{
     initEnter()
   })
+  
   const initEnter = ()=>{
     document.addEventListener('keyup',e=>{
       if(e.key === 'Enter') router.push({name:"edit"})
@@ -124,6 +136,9 @@
       .card-skill-item {
         margin: 0 10px;
       }
+    }
+    .tag {
+      margin: 3px;
     }
   }
   </style>
